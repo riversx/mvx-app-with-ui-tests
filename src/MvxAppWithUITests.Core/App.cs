d9d228a7529b263ee495c40f5f46865d1288d5a3
Boris Sclauzero <boris.sclauzero@fd2-solutions.it>
2022-11-15 21:17:47 +0100

    initial commit with mvx scaffolding

---
+++ src/MvxAppWithUITests.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using MvxAppWithUITests.Core.ViewModels.Home;
+
+namespace MvxAppWithUITests.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
---
+++ src/MvxAppWithUITests.Core/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvxAppWithUITests.Core.ViewModels.Home
+{
+    public class HomeViewModel : BaseViewModel
+    {
+    }
+}
